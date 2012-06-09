9ac592b9a96c4629a496d5278e64f06fb357cfc7
Aymeric Fabian <aymeric.fabian@gmail.com>
2012-06-09 02:56:50 +0200

    test dans main

--- editeur/src/main.cpp
+++ editeur/src/main.cpp
@@ -5,8 +5,5 @@
 int main()
 {
     edt::Repere *r = new edt::Repere(5,8);
-    std::cout << r->largeur << " " << r->hauteur << std::endl;;
-    r->redimensionner(3,6);
-    std::cout << r->largeur << " " << r->hauteur << std::endl;;
     return EXIT_SUCCESS;
 }
