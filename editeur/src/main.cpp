ca33ab3c9b26161d4a57946cb87f619ef723562a
Matthieu Zimmer <zimme0511@etumail.uhp-nancy.fr>
2012-06-19 00:00:32 +0200

    nettoyage du main

--- editeur/src/main.cpp
+++ editeur/src/main.cpp
@@ -1,14 +1,8 @@
 #include <iostream>
 #include "edt/DaGame.hpp"
 
-#include "bib/XMLEngine.hpp"
-
 int main()
 {
-    cce::Carte c;
-    bib::XMLEngine::save<cce::Carte>(c, "Carte", "test.bidon");
-  
-  
     edt::DaGame game;
     game.gameloop();
 
