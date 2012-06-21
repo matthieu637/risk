f3b0ae7154d6af8dd949ee75a0282e93cfd48d42
Aymeric Fabian <aymeric.fabian@gmail.com>
2012-06-21 04:48:42 +0200

    pays modele finito

--- communsce/include/cce/Pays.hpp
+++ communsce/include/cce/Pays.hpp
@@ -5,7 +5,7 @@
 #include <list>
 #include <boost/serialization/nvp.hpp>
 #include <boost/serialization/map.hpp> 
-#include "Region.hpp"
+#include "cce/Region.hpp"
 #include "cce/PointSpawn.hpp"
 
 using std::string;
@@ -13,7 +13,6 @@ using std::list;
 using boost::serialization::make_nvp; 
 
 namespace cce{
-class PointSpawn;
 class Pays
 {
 
@@ -25,55 +24,53 @@ virtual ~Pays();
 ///\brief income du pays
 ///\return Retourne l'income du pays
 ///
-	    int getIncome();
+    int getIncome();
 
 ///
 ///\brief nom du pays
 ///\return Retourne le nom du pays
 ///
-	    string getNom();
+    string getNom();
 
-	    
+    
 ///\brief regions du pays
 ///\return Retourne les regions du pays
 ///
-            map<string,Region>* getRegions();
-	    
+    map<string,Region>* getRegions();
+    
 ///\brief obtenir une région
 ///\return Retourne une région associée à l'argument 
 ///
-            Region getRegion(string nom);
-	    
+    Region getRegion(string nom);
+    
 ///	    
 ///
 ///\brief point de spawn du pays
 ///\return Retourne le point de spawn du pays
 ///
-            PointSpawn* getPointSpawn();
-	    
+    PointSpawn* getPointSpawn();
+    
 ///
 ///\brief ajoute une région à la liste des régions
 ///\param r : regions a associer
 ///
-            void addRegion(const string& name, const Region& r);  
-	    
-
+    void addRegion(const string& name, const Region& r);  
+    
     template<class Archive>
     void serialize(Archive& ar, const unsigned int version)
     {
 	(void) version;
-        ar & make_nvp("nom", nom); 
+	ar & make_nvp("nom", nom); 
 	ar & make_nvp("income", income);
 	ar & make_nvp("PointSpawn", ps);
 	ar & make_nvp("Regions", regions);
     }
     
 protected : 
-	    int income;
-	    string nom;
-	    map<string,Region> regions;
-	    PointSpawn ps;	  
-
+    int income;
+    string nom;
+    map<string,Region> regions;
+    PointSpawn* ps;
 };
 }
 #endif // PAYS_HPP
